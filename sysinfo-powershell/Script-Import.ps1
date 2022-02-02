634a3f0d1fbf3e055249eb9e5cc77a9e78f97beb
Wayde B <wbstwayde@gmail.com>
2022-02-02 16:10:51 +1100

    Add files via upload

---
+++ sysinfo-powershell/Script-Import.ps1
@@ -0,0 +1,27 @@
+﻿function Import-Scripts {
+
+    param (
+        [switch]$userinfo,
+        [switch]$win32info,
+        [switch]$sysinfo,
+        [switch]$all
+    )
+
+    begin {
+        if ($userinfo.IsPresent) {
+            Copy-Item -Path $HOME\utils\sysinfo-powershell\scripts\user-info.ps1 -Destination $HOME\Documents\WindowsPowerShell\Modules
+        }
+
+        if ($win32info.IsPresent) {
+            Copy-Item -Path $HOME\utils\sysinfo-powershell\scripts\win32-info.ps1 -Destination $HOME\Documents\WindowsPowerShell\Modules
+        }
+
+        if ($sysinfo.IsPresent) {
+            Copy-Item -Path $HOME\utils\sysinfo-powershell\scripts\sys-info.ps1-Destination $HOME\Documents\WindowsPowerShell\Modules
+        }
+
+        if ($all.IsPresent) {
+            Copy-Item -Path $HOME\utils\sysinfo-powershell\scripts\*.ps1 -Destination $HOME\Documents\WindowsPowerShell\Modules
+        }
+    }  
+}
