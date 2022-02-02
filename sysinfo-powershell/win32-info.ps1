125c5242693a320e04573fb8409e0f8dcbbc84b3
Wayde B <git@alphaxdev.xyz>
2022-02-02 15:31:16 +1100

    Update and rename system-info.ps1 to win32-info.ps1

---
+++ sysinfo-powershell/win32-info.ps1
@@ -0,0 +1,11 @@
+﻿function Get-Win32Info {
+    param (
+        $win32,
+
+    )
+
+    foreach ($Win32 in $win32) {
+        $Args = Get-Command -ParameterName $Win32 -ErrorAction SilentlyContinue
+        Get-WmiObject -Query "select * from win32_operatingsystem" | Select-Object -Expand $Win32
+    }
+}
