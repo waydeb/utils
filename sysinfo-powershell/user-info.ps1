ac0598a6e7769774c4de285b6e4671f45f2bc668
Wayde B <git@alphaxdev.xyz>
2022-02-02 16:10:31 +1100

    Delete sysinfo-powershell directory

--- sysinfo-powershell/user-info.ps1
+++
@@ -1,9 +0,0 @@
-function Get-UserInfo {
-    params (
-        $acc
-    )
-    foreach ($Acc in $acc) {
-        $Args = Get-Command -ParameterName $Acc -ErrorAction SilentlyContinue
-        Get-WmiObject -Query "select * from Win32_UserAccount where Name='$env:USERNAME'" | Select-Object -Expand $Acc
-    }
-}
