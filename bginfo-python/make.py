f87a37e65ae5c2e78db458cc4e527d8d66d769d8
Wayde Blazely <git@alphaxdev.xyz>
2024-09-16 20:48:29 +1000

    holy shit im alive?
    yeah i know its crazy, i kinda have a job though, anyways ive fixed this because this code didnt work lol

--- bginfo-python/make.py
+++ bginfo-python/make.py
@@ -3,8 +3,8 @@ import asyncio
 import ctypes as c
 import json as j
 from PIL import Image, ImageDraw, ImageFont
-
 with open("C:\\auto_scripts\\wallpaper\\required\\settings.json") as f:
+
     settings  = j.load(f)
     x_adjust  = settings["image_settings"]["x_adjust"]
     y_adjust  = settings["image_settings"]["y_adjust"]
@@ -18,30 +18,30 @@ draw = ImageDraw.Draw(im)
 
 
 async def main():
-    print("Starting wallpaper script")
-    full = f"""
-Hostname: {await si.hostname()}
-OEM & Model: {await si.oem()}
-Version: {await si.version()}
-CPU: {await si.specs()}
-RAM: {await si.ram()}
-Disk: {await si.disk()}
-Network: {await si.network()}
-Boot Time: {await si.boot()}
-"""
-    print(full+'\nSystem Info at login time')
-    print(f"Starting wallpaper creation\nSource image: {base}")
-    font = ImageFont.truetype(text_font, 20)
-    textwidth, textheight = draw.textsize(full, font)
-    margin = 10
-    x = width - textwidth - margin - x_adjust
-    y = height - textheight - margin - y_adjust
-    draw.text((x,y), full, font=font)
-    im.save(wallpaper)
-    print(f"Wallpaper saved, waiting 3 seconds before applying\nFile location: {wallpaper}")
-    await asyncio.sleep(3)
-    c.windll.user32.SystemParametersInfoW(20, 0, wallpaper, 3)
-    print("Wallpaper applied")
-
+    try:
+        print(type(im))
+        print("Starting wallpaper script")
+        full = f"""
+    Hostname: {await si.hostname()}
+    OEM & Model: {await si.oem()}
+    Version: {await si.version()}
+    CPU: {await si.specs()}
+    RAM: {await si.ram()}
+    Disk: {await si.disk()}
+    Network: {await si.network()}
+    Boot Time: {await si.boot()}
+    """
+        print(full+'\nSystem Info at login time')
+        print(f"Starting wallpaper creation\nSource image: {base}")
+        font_size=20
+        font = ImageFont.truetype(text_font, font_size)
+        draw.multiline_text((x_adjust,y_adjust), text=full, font=font)
+        im.save(wallpaper)
+        print(f"Wallpaper saved, waiting 3 seconds before applying\nFile location: {wallpaper}")
+        await asyncio.sleep(3)
+        c.windll.user32.SystemParametersInfoW(20, 0, wallpaper, 3)
+        print("Wallpaper applied")
+    except Exception as e:
+        return e
 
 asyncio.run(main())
